44eca170282995700fa14bdd1643164b4eabe385
10CDizzle <chris@genone.tech>
2022-02-08 22:17:24 -0500

    Modified example code should print to UART0

--- HelloWorldnrf/src/main.rs
+++ HelloWorldnrf/src/main.rs
@@ -1,17 +1,57 @@
 #![no_main]
 #![no_std]
 
-use defmt_rtt as _;
 use panic_halt as _;
 
-use cortex_m_rt::entry;
-use embedded_hal::blocking::delay::DelayMs;
-use microbit::hal::{clocks::Clocks, timer::Timer};
-use rubble::beacon::Beacon;
-use rubble::link::{ad_structure::AdStructure, CompanyId, MIN_PDU_BUF};
-use rubble_nrf5x::radio::{BleRadio, PacketBuffer};
-use rubble_nrf5x::utils::get_device_address;
+use core::fmt::Write;
 
-//Main entry point into program
-fn main() {
+#[cfg(feature = "v1")]
+use microbit::{
+    hal::prelude::*,
+    hal::uart,
+    hal::uart::{Baudrate, Parity},
+};
+
+#[cfg(feature = "v2")]
+use microbit::{
+    hal::prelude::*,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
+};
+
+use cortex_m_rt::entry;
+
+#[cfg(feature = "v2")]
+mod serial_setup;
+#[cfg(feature = "v2")]
+use serial_setup::UartePort;
+
+#[entry]
+fn main() -> ! {
+    let board = microbit::Board::take().unwrap();
+
+    #[cfg(feature = "v1")]
+    let mut serial = {
+        uart::Uart::new(
+            board.UART0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        )
+    };
+
+    #[cfg(feature = "v2")]
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+
+    loop {
+        write!(serial, "Hello World:\r\n").unwrap();
+    }
 }
