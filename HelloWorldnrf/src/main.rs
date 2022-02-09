866c90265bf3d56a6c2acc53308e9d2d3e160d70
10CDizzle <chris@genone.tech>
2022-02-08 23:05:51 -0500

    Successfully prints "hello world" from example code in microbit repo

--- HelloWorldnrf/src/main.rs
+++ HelloWorldnrf/src/main.rs
@@ -3,55 +3,40 @@
 
 use panic_halt as _;
 
-use core::fmt::Write;
-
-#[cfg(feature = "v1")]
-use microbit::{
-    hal::prelude::*,
-    hal::uart,
-    hal::uart::{Baudrate, Parity},
-};
-
-#[cfg(feature = "v2")]
-use microbit::{
-    hal::prelude::*,
-    hal::uarte,
-    hal::uarte::{Baudrate, Parity},
-};
-
 use cortex_m_rt::entry;
-
-#[cfg(feature = "v2")]
-mod serial_setup;
-#[cfg(feature = "v2")]
-use serial_setup::UartePort;
 
 #[entry]
 fn main() -> ! {
-    let board = microbit::Board::take().unwrap();
+    if let Some(p) = microbit::Peripherals::take() {
+        p.GPIO.pin_cnf[24].write(|w| w.pull().pullup().dir().output());
+        p.GPIO.pin_cnf[25].write(|w| w.pull().disabled().dir().input());
 
-    #[cfg(feature = "v1")]
-    let mut serial = {
-        uart::Uart::new(
-            board.UART0,
-            board.uart.into(),
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        )
-    };
+        p.UART0.pseltxd.write(|w| unsafe { w.bits(24) });
+        p.UART0.pselrxd.write(|w| unsafe { w.bits(25) });
 
-    #[cfg(feature = "v2")]
-    let mut serial = {
-        let serial = uarte::Uarte::new(
-            board.UARTE0,
-            board.uart.into(),
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        );
-        UartePort::new(serial)
-    };
+        p.UART0.baudrate.write(|w| w.baudrate().baud115200());
+        p.UART0.enable.write(|w| w.enable().enabled());
+
+        let _ = write_uart0(&p.UART0, "Hello World!\n");
+    }
 
     loop {
-        write!(serial, "Hello World:\r\n").unwrap();
+        continue;
     }
+}
+
+fn write_uart0(uart0: &microbit::pac::UART0, s: &str) -> core::fmt::Result {
+    uart0.tasks_starttx.write(|w| unsafe { w.bits(1) });
+    for c in s.as_bytes() {
+        /* Write the current character to the output register */
+        uart0.txd.write(|w| unsafe { w.bits(u32::from(*c)) });
+
+        /* Wait until the UART is clear to send */
+        while uart0.events_txdrdy.read().bits() == 0 {}
+
+        /* And then set it back to 0 again, just because ?!? */
+        uart0.events_txdrdy.write(|w| unsafe { w.bits(0) });
+    }
+    uart0.tasks_stoptx.write(|w| unsafe { w.bits(1) });
+    Ok(())
 }
